f914286877d3b24f56796ec7297a382ab195ba98
ftrbnd <giosalas25@gmail.com>
2023-05-31 13:44:13 -0700

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
     return (
-        <div>
+        <div className={styles.Footer}>
             Keebtown poster by
             <a href='https://mykeyboard.eu/catalogue/keeb-town-poster_5222/' target='_blank' rel="noreferrer">
-                mykeyboard.eu
+                {' '}mykeyboard.eu
             </a>
         </div>
     );
