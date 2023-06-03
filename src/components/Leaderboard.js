359cd6a1e4e6ac40f85246e6ca4f07be75412697
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-06-02 23:44:57 -0700

    Implement leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useRef } from 'react';
+import { firestore } from '../utils/firebase';
+import styles from '../styles/Leaderboard.module.css';
 
 const Leaderboard = () => {
+    const leaderboard = useRef([]);
+
+    useEffect(() => {
+        const data = [];
+
+        async function fetchLeaderboard() {
+            const querySnapshot = await getDocs(collection(firestore, 'leaderboard'));
+            querySnapshot.forEach(doc => {
+                data.push(doc.data());
+            });
+        }
+
+        fetchLeaderboard();
+        // sort data by shortest time:
+        // data.sort((a, b) => (a.seconds > b.seconds) ? 1 : -1);
+        leaderboard.current = data;
+    }, []);
+
     return (
-        <div>
+        <div className={styles.Leaderboard}>
             <h2>Leaderboard</h2>
-            <ol>
-                <li>first</li>
-            </ol>
+            <ol className={styles.data}> {
+                leaderboard.current.map(entry =>
+                    <li key={entry.date.seconds} className={styles.entry}>
+                        <p>Nickname: {entry.nickname}</p>
+                        <p>Time: {entry.seconds}</p>
+                        <p>Date: {entry.date.seconds}</p>
+                    </li>
+                )
+            } </ol>
         </div>
     );
 };
