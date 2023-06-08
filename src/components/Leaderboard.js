517645bfe9482c4a83bfe0bd426e97b23fff5f9a
ftrbnd <giosalas25@gmail.com>
2023-06-08 15:14:33 -0700

    Switch to reducer to manage state and use tab component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,60 +1,90 @@
 import { collection, getDocs } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { firestore } from '../utils/firebase';
 import styles from '../styles/Leaderboard.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import LeaderboardTab from './LeaderboardTab';
+
+const ACTIONS = {
+    CENTRAL_PARK: 'Central Park',
+    GREEN_GOBLIN: 'Green Goblin',
+    MISTER_NEGATIVE: 'Mister Negative',
+    VULTURE: 'Vulture'
+};
+
+function reducer(leaderboard, action) {
+    switch (action.type) {
+        case ACTIONS.CENTRAL_PARK:
+            return {
+                ...leaderboard,
+                centralPark: [
+                    ...leaderboard.centralPark
+                        .filter(data => data.date.seconds !== action.payload.data.date.seconds),
+                    action.payload.data
+                ].sort((a, b) => (a.seconds > b.seconds) ? 1 : -1)
+            };
+        case ACTIONS.GREEN_GOBLIN:
+            return {
+                ...leaderboard,
+                greenGoblin: [
+                    ...leaderboard.greenGoblin
+                        .filter(data => data.date.seconds !== action.payload.data.date.seconds),
+                    action.payload.data
+                ].sort((a, b) => (a.seconds > b.seconds) ? 1 : -1)
+            };
+        case ACTIONS.MISTER_NEGATIVE:
+            return {
+                ...leaderboard,
+                misterNegative: [
+                    ...leaderboard.misterNegative
+                        .filter(data => data.date.seconds !== action.payload.data.date.seconds),
+                    action.payload.data
+                ].sort((a, b) => (a.seconds > b.seconds) ? 1 : -1)
+            };
+        case ACTIONS.VULTURE:
+            return {
+                ...leaderboard,
+                vulture: [
+                    ...leaderboard.vulture
+                        .filter(data => data.date.seconds !== action.payload.data.date.seconds),
+                    action.payload.data
+                ].sort((a, b) => (a.seconds > b.seconds) ? 1 : -1)
+            };
+        default:
+    }
+}
 
 const Leaderboard = () => {
-    const [leaderboard, setLeaderboard] = useState([]);
+    const [leaderboard, dispatch] = useReducer(reducer, {
+        centralPark: [],
+        greenGoblin: [],
+        misterNegative: [],
+        vulture: []
+    });
 
     useEffect(() => {
         async function fetchLeaderboard() {
-            const data = [];
-
             const querySnapshot = await getDocs(collection(firestore, 'leaderboard'));
             querySnapshot.forEach(doc => {
-                data.push(doc.data());
+                dispatch({ type: doc.data().image, payload: { data: doc.data()} });
             });
-
-            return data;
         }
 
-        fetchLeaderboard().then(data => {
-            data.sort((a, b) => (a.seconds > b.seconds) ? 1 : -1);
-            setLeaderboard(data);
-        });
+        fetchLeaderboard();
     }, []);
-
-    const formattedTime = (seconds) => {
-        const minutes = Math.floor(seconds / 60);
-        const secs = Math.floor(seconds % 60);
-        
-        return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
-    };
 
     return (
         <div className={styles.Leaderboard}>
             <h2>Leaderboard</h2>
-            <div className={styles.columnNames}>
-                <h3>Nickname</h3>
-                <h3>Time</h3>
-                <h3>Image</h3>
-                <h3>Date</h3>
-            </div>
-            <ol className={styles.data}>
-                {leaderboard.map(entry =>
-                    <li key={entry.date.seconds} className={styles.entry}>
-                        <div className={styles.user}>
-                            {entry.avatar ? <img className={styles.userAvatar} src={entry.avatar} alt='custom user avatar' /> : <FontAwesomeIcon icon={faUser} />}
-                            <p>{entry.nickname}</p>
-                        </div>
-                        <p>{formattedTime(entry.seconds)}</p>
-                        <p>{ entry.image }</p>
-                        <p>{new Date(entry.date.toDate()).toLocaleString()}</p>
-                    </li>
-                )}
-            </ol>
+            <ul className={styles.tabs}>
+                <li>Central Park</li>
+                <li>Green Goblin</li>
+                <li>Mister Negative</li>
+                <li>Vulture</li>
+            </ul>
+            <LeaderboardTab data={leaderboard.centralPark} />
+            <LeaderboardTab data={leaderboard.greenGoblin} />
+            <LeaderboardTab data={leaderboard.misterNegative} />
+            <LeaderboardTab data={leaderboard.vulture} />
         </div>
     );
 };
---
+++ src/components/LeaderboardTab.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from '../styles/LeaderboardTab.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+function formattedTime (seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    
+    return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+};
+
+const LeaderboardTab = ({ data }) => {
+    return (
+        <>
+            <div className={styles.columnNames}>
+                <h3>Nickname</h3>
+                <h3>Time</h3>
+                <h3>Image</h3>
+                <h3>Date</h3>
+            </div>
+            <ol className={styles.data}>
+                {data.map(entry =>
+                    <li key={entry.date.seconds} className={styles.entry}>
+                        <div className={styles.user}>
+                            {entry.avatar ? <img className={styles.userAvatar} src={entry.avatar} alt='custom user avatar' /> : <FontAwesomeIcon icon={faUser} />}
+                            <p>{entry.nickname}</p>
+                        </div>
+                        <p>{formattedTime(entry.seconds)}</p>
+                        <p>{ entry.image }</p>
+                        <p>{new Date(entry.date.toDate()).toLocaleString()}</p>
+                    </li>
+                )}
+            </ol>
+        </>
+    );
+};
+
+export default LeaderboardTab;
